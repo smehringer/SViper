b0eda447ca1033f0eb85aa4f737e5bd0629b30e1
smehringer <svenja.mehringer@gmail.com>
2018-02-22 14:40:26 +0100

    [FIX] fixes merging alignments by turning hard clipping to soft clipping and making the primary and supplementary sequence to be the same.

--- include/merge_split_alignments.h
+++ include/merge_split_alignments.h
@@ -14,7 +14,7 @@ int compute_map_end_pos(unsigned map_begin_pos,
 {
     int len{0}; // tracks read length so far
     for (auto ce : cigar) //
-        if (ce.operation & 4) // D or M, not I nor S
+        if (ce.operation & 4) // either D or M, not I,S,H
             len += ce.count;
     return map_begin_pos + len;
 }
@@ -34,7 +34,6 @@ unsigned get_read_begin_and_alter_cigar(BamAlignmentRecord & record)
     unsigned read_begin_pos{0};
     if ((record.cigar[0]).operation == 'H') // hard clipping
     {
-        read_begin_pos += (record.cigar[0]).count;
         erase(record.cigar, 0);
     }
     if ((record.cigar[0]).operation == 'S') // soft clipping
@@ -51,10 +50,8 @@ unsigned get_read_end_and_alter_cigar(BamAlignmentRecord & record)
 
     if ((record.cigar[length(record.cigar) - 1]).operation == 'H') // hard clipping
     {
-        read_end_pos -= (record.cigar[length(record.cigar) - 1]).count;
         erase(record.cigar, length(record.cigar) - 1);
     }
-
     if ((record.cigar[length(record.cigar) - 1]).operation == 'S') // soft clipping
     {
         read_end_pos -= (record.cigar[length(record.cigar) - 1]).count;
@@ -99,11 +96,12 @@ void truncate_cigar_right(BamAlignmentRecord & record, int until)
     if ((record.cigar[length(record.cigar) - 1]).operation != 'M' &&
         (record.cigar[length(record.cigar) - 1]).operation != 'I') // meaning == H,S or D
     {
-        if ((record.cigar[length(record.cigar) - 1]).operation == 'D')
-        {   // continuing deletion in record must be added to the laarge DEL event
+        if ((record.cigar[length(record.cigar) - 1]).operation == 'D' ||
+            (record.cigar[length(record.cigar) - 1]).operation == 'H')
+        {   // continuing deletion in record must be added to the large DEL event
             erase(record.cigar, length(record.cigar) - 1);
         }
-        else // meaning == H or S
+        else // meaning == S
         {
             (record.cigar[length(record.cigar) - 1]).operation = 'I'; // add those bases
         }
@@ -190,7 +188,7 @@ void truncate_cigar_left(BamAlignmentRecord & record, int until)
         (record.cigar[0]).operation != 'I') // meaning == H,S or D
     {
         if ((record.cigar[0]).operation == 'D')
-        {   // continuing deletion in record must be added to the laarge DEL event
+        {   // continuing deletion in record must be added to the large DEL event
             num_truncated_bases += (record.cigar[0]).count;
             erase(record.cigar, 0);
         }
@@ -236,11 +234,126 @@ void transform_cigar_left_into_insertion(BamAlignmentRecord & record, int until)
 
     // count all events from cigar pos to have one big insertion
     for (unsigned idx = 0; idx < cigar_pos; ++idx)
-        if ((record.cigar[idx]).operation != 'D') // only add isnerted and matched bases
+        if ((record.cigar[idx]).operation != 'D') // only add inserted and matched bases
             insertion_size += (record.cigar[idx]).count;
 
     erase(record.cigar, 0, cigar_pos); // erase the overlapping beginning,
     insert(record.cigar, 0, CigarElement<char, unsigned>('I', insertion_size));
+}
+
+void turn_hard_clipping_to_soft_clipping(BamAlignmentRecord & prim, BamAlignmentRecord & supp)
+{
+    decltype(prim.seq) final_prim_seq;
+    decltype(supp.seq) final_supp_seq;
+    // Note: common hard clipping is removed from the cigar string
+
+    // beginning of alignment
+    if ((prim.cigar[0]).operation == 'H')
+    {
+        if ((supp.cigar[0]).operation == 'H')
+        { // both have hard clipping at the beginning
+            int shared = std::min((prim.cigar[0]).count,(supp.cigar[0]).count);
+            SEQAN_ASSERT(shared != 0); // would imply that the cigar entry is H:0 somewhere
+            (prim.cigar[0]).count -= shared;
+            (supp.cigar[0]).count -= shared;
+
+            if ((prim.cigar[0]).count == 0)
+            {
+                erase(prim.cigar, 0);
+                final_prim_seq = prim.seq;
+            }
+            else
+            {
+                (prim.cigar[0]).operation = 'S';
+                append(final_prim_seq, prefix(supp.seq, (prim.cigar[0]).count));
+                append(final_prim_seq, prim.seq);
+            }
+
+            if ((supp.cigar[0]).count == 0)
+            {
+                erase(supp.cigar, 0);
+                final_supp_seq = supp.seq;
+            }
+            else
+            {
+                (supp.cigar[0]).operation = 'S';
+                append(final_supp_seq, prefix(prim.seq, (supp.cigar[0]).count));
+                append(final_supp_seq, supp.seq);
+            }
+        }
+        else
+        { // supp has no hard clipping at the beginning but prim has
+            (prim.cigar[0]).operation = 'S';
+            append(final_prim_seq, prefix(supp.seq, (prim.cigar[0]).count));
+            append(final_prim_seq, prim.seq);
+            final_supp_seq = supp.seq;
+        }
+    }
+    else
+    {
+        if ((supp.cigar[0]).operation == 'H')
+        { // prim has no hard clipping at the beginning but supp has
+            (supp.cigar[0]).operation = 'S';
+            append(final_supp_seq, prefix(prim.seq, (supp.cigar[0]).count));
+            append(final_supp_seq, supp.seq);
+            final_prim_seq = prim.seq;
+        }
+        else
+        { // both have no hard clipping at the beginning
+            final_prim_seq = prim.seq;
+            final_supp_seq = supp.seq;
+        }
+    }
+
+    // end of alignment
+    auto & prim_ce = prim.cigar[length(prim.cigar) - 1];
+    auto & supp_ce = supp.cigar[length(supp.cigar) - 1];
+    if (prim_ce.operation == 'H')
+    {
+        if (supp_ce.operation == 'H')
+        { // both have hard clipping at the beginning
+            int shared = std::min(prim_ce.count,supp_ce.count);
+            SEQAN_ASSERT(shared != 0); // would imply that the cigar entry is H:0 somewhere
+            prim_ce.count -= shared;
+            supp_ce.count -= shared;
+
+            if (prim_ce.count == 0)
+            {
+                erase(prim.cigar, length(prim.cigar) - 1);
+            }
+            else
+            {
+                prim_ce.operation = 'S';
+                append(final_prim_seq, suffix(supp.seq, length(supp.seq) - prim_ce.count));
+            }
+
+            if (supp_ce.count == 0)
+            {
+                erase(supp.cigar, length(supp.cigar) - 1);
+            }
+            else
+            {
+                supp_ce.operation = 'S';
+                append(final_supp_seq, suffix(prim.seq, length(prim.seq) - supp_ce.count));
+            }
+        }
+        else
+        { // supp has no hard clipping at the beginning but prim has
+            prim_ce.operation = 'S';
+            append(final_prim_seq, suffix(supp.seq, length(supp.seq) - prim_ce.count));
+        }
+    }
+    else
+    {
+        if (supp_ce.operation == 'H')
+        { // prim has no hard clipping at the beginning but supp has
+            supp_ce.operation = 'S';
+            append(final_supp_seq, suffix(prim.seq, length(prim.seq) - supp_ce.count));
+        }
+    }
+
+    prim.seq = final_prim_seq;
+    supp.seq = final_supp_seq;
 }
 
 BamAlignmentRecord merge_record_group(vector<BamAlignmentRecord> & record_group)
@@ -257,14 +370,11 @@ BamAlignmentRecord merge_record_group(vector<BamAlignmentRecord> & record_group)
 #ifndef NDEBUG
         if (record_group.size() > 1)
         {
-            std::cout << "Merging group of " << record_group.size() << " with name " << final_record.qName << "\t" << endl;
+            log_file << "Merging group of " << record_group.size() << " with name " << final_record.qName << "\t" << endl;
             for (auto const & rec : record_group)
-                cerr << "  -> "<< rec.qName << " " << rec.flag << " " << rec.rID << " " << rec.beginPos << " " << rec.mapQ << endl;
+                log_file << "  -> "<< rec.qName << " " << rec.flag << " " << rec.rID << " " << rec.beginPos << " " << rec.mapQ << endl;
         }
 #endif
-
-        if (final_record.qName == "final_chr1_4939457_92")
-            std::cerr << "oh oh";
 
     for (unsigned i = 1; i < record_group.size(); ++i)
     {
@@ -284,10 +394,13 @@ BamAlignmentRecord merge_record_group(vector<BamAlignmentRecord> & record_group)
         if (supp_record.beginPos < prim_record.beginPos)
         { // ---supp-- before --final--
 #ifndef NDEBUG
-                cerr << "  -> ---supp-- before --final--" << endl;
+                log_file << "  -> ---supp-- before --final--" << endl;
 #endif
             if (compute_map_end_pos(final_record.beginPos, final_record.cigar) <= compute_map_end_pos(supp_record.beginPos, supp_record.cigar))
                 continue; // do not merge supp alignments that are completely covered
+
+            turn_hard_clipping_to_soft_clipping(prim_record, supp_record);
+            SEQAN_ASSERT(length(prim_record.seq) == length(supp_record.seq));
 
             // Alter cigar string
             // 1) remove soft clipping at the left end from the primary (right) alignment
@@ -318,14 +431,18 @@ BamAlignmentRecord merge_record_group(vector<BamAlignmentRecord> & record_group)
             // replace final variables
             prim_record.beginPos = supp_record.beginPos;
             prim_record.cigar = supp_record.cigar;
+
         }
         else if (prim_record.beginPos < supp_record.beginPos)
         { // ---final-- before --supp--
 #ifndef NDEBUG
-                cerr << "  -> ---final-- before --supp--" << endl;
+                log_file << "  -> ---final-- before --supp--" << endl;
 #endif
             if (compute_map_end_pos(final_record.beginPos, final_record.cigar) >= compute_map_end_pos(supp_record.beginPos, supp_record.cigar))
                 continue; // do not merge supp alignments that are completely covered
+
+            turn_hard_clipping_to_soft_clipping(prim_record, supp_record);
+            SEQAN_ASSERT(length(prim_record.seq) == length(supp_record.seq));
 
             // Alter cigar string
             // 1) remove soft clipping at the right end from the primary (left) alignment
