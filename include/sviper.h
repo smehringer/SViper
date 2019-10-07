f980693957a930c4c5dc407e204162ec62e971a4
Joshua Kim <joshua.kim@fu-berlin.de>
2019-10-07 16:26:16 +0200

    Move argument_parser stuff into sviper.h

--- include/sviper.h
+++ include/sviper.h
@@ -37,6 +37,110 @@ struct file_info{
     std::vector<seqan::BamAlignmentRecord>  polished_reads;
 };
 
+ArgumentParser::ParseResult parseCommandLine(CmdOptions & options, int argc, char const ** argv)
+{
+    // Setup ArgumentParser.
+    seqan::ArgumentParser parser("SViper");
+    setVersion(parser, "2.0.0");
+
+    addOption(parser, seqan::ArgParseOption(
+        "c", "candidate-vcf",
+        "A structural variant vcf file (with e.g. <DEL> tags), containing the potential variant sites to be looked at.",
+        seqan::ArgParseArgument::INPUT_FILE, "VCF_FILE"));
+
+    addOption(parser, seqan::ArgParseOption(
+        "s", "short-read-bam",
+        "The indexed bam file containing short used for polishing at variant sites.",
+        seqan::ArgParseArgument::INPUT_FILE, "BAM_FILE"));
+
+    addOption(parser, seqan::ArgParseOption(
+        "l", "long-read-bam",
+        "The indexed bam file containing long reads to be polished at variant sites.",
+        seqan::ArgParseArgument::INPUT_FILE, "BAM_FILE"));
+
+    addOption(parser, seqan::ArgParseOption(
+        "r", "reference",
+        "The indexed (fai) reference file.",
+        seqan::ArgParseArgument::INPUT_FILE, "FA_FILE"));
+
+    addOption(parser, seqan::ArgParseOption(
+        "t", "threads",
+        "The threads to use.",
+        seqan::ArgParseArgument::INTEGER, "INT"));
+
+    addOption(parser, seqan::ArgParseOption(
+        "k", "flanking-region",
+        "The flanking region in bp's around a breakpoint to be considered for polishing",
+        seqan::ArgParseArgument::INTEGER, "INT"));
+
+    addOption(parser, seqan::ArgParseOption(
+        "x", "coverage-short-reads",
+        "The original short read mean coverage. This value is used to restrict short read coverage on extraction to avoid mapping bias",
+        seqan::ArgParseArgument::INTEGER, "INT"));
+
+    addOption(parser, seqan::ArgParseOption(
+        "", "median-ins-size-short-reads",
+        "The median of the short read insert size (end of read1 until beginning of read2). "
+        "This value is used to compute a threshold for error correction.",
+        seqan::ArgParseArgument::INTEGER, "INT"));
+
+    addOption(parser, seqan::ArgParseOption(
+        "", "stdev-ins-size-short-reads",
+        "The median of the short read insert size (end of read1 until beginning of read2). "
+        "This value is used to compute a threshold for error correction..",
+        seqan::ArgParseArgument::INTEGER, "INT"));
+
+    addOption(parser, seqan::ArgParseOption(
+        "o", "output-prefix",
+        "A name for the output files. The current output is a log file and vcf file, that contains the "
+        "polished sequences for each variant.",
+        seqan::ArgParseArgument::INPUT_FILE, "PREFIX"));
+
+    addOption(parser, seqan::ArgParseOption(
+        "v", "verbose",
+        "Turn on detailed information about the process."));
+
+    addOption(parser, seqan::ArgParseOption(
+        "", "output-polished-bam", "For debugging or manual inspection the polished reads can be written to a file."));
+
+    setRequired(parser, "c");
+    setRequired(parser, "l");
+    setRequired(parser, "s");
+    setRequired(parser, "r");
+
+    setMinValue(parser, "k", "50");
+    setMaxValue(parser, "k", "1000");
+    setDefaultValue(parser, "k", "400");
+
+    setDefaultValue(parser, "t", std::thread::hardware_concurrency());
+
+    // Parse command line.
+    seqan::ArgumentParser::ParseResult res = seqan::parse(parser, argc, argv);
+
+    // Only extract  options if the program will continue after parseCommandLine()
+    if (res != seqan::ArgumentParser::PARSE_OK)
+        return res;
+
+    // Extract option values.
+    getOptionValue(options.candidate_file_name, parser, "candidate-vcf");
+    getOptionValue(options.long_read_file_name, parser, "long-read-bam");
+    getOptionValue(options.short_read_file_name, parser, "short-read-bam");
+    getOptionValue(options.reference_file_name, parser, "reference");
+    getOptionValue(options.output_prefix, parser, "output-prefix");
+    getOptionValue(options.flanking_region, parser, "flanking-region");
+    getOptionValue(options.mean_coverage_of_short_reads, parser, "coverage-short-reads");
+    getOptionValue(options.mean_insert_size_of_short_reads, parser, "median-ins-size-short-reads");
+    getOptionValue(options.stdev_insert_size_of_short_reads, parser, "stdev-ins-size-short-reads");
+    getOptionValue(options.threads, parser, "threads");
+    options.verbose = isSet(parser, "verbose");
+    options.output_polished_bam = isSet(parser, "output-polished-bam");
+
+    if (options.output_prefix.empty())
+        options.output_prefix = options.candidate_file_name + "_polished";
+
+    return seqan::ArgumentParser::PARSE_OK;
+}
+
 bool read_vcf(std::vector<Variant> & variants, std::vector<std::string> & vcf_header, file_info * info)
 {
     ifstream input_vcf;           // The candidate variants to polish
